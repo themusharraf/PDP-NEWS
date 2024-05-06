ea0f544c66de6ee484de9a11ba35d306fbc97b87
msi <meibrohimov.@gmail.com>
2024-05-06 10:40:41 +0500

    Home Contact page change👻

--- root/root/settings.py
+++ root/root/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-w4%8w3v47b2vg62!41*eaw6w0l$63k1g#0v3f^(gz2)bcmfp$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
